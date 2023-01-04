b3e3c1aa4850acd73c67e519f266139835439928
pwdjnyz11 <jyjyjy7418@gmail.com>
2023-01-04 15:11:36 +0900

    update commit

--- app/src/main/java/org/techtown/miniproject/MainActivity.java
+++ app/src/main/java/org/techtown/miniproject/MainActivity.java
@@ -1,8 +1,17 @@
 package org.techtown.miniproject;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView rank_btn = (ImageView) findViewById(R.id.show_rank_button);
+        rank_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RankActivity.class);
+                startActivityResult.launch(intent);
+            }
+        });
+
     }
+
+    ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+
+                    }
+                }
+            });
 }
---
+++ app/src/main/java/org/techtown/miniproject/RankActivity.java
@@ -0,0 +1,13 @@
+package org.techtown.miniproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RankActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rank);
+    }
+}
