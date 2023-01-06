0fe981ca4603a8241088c13d26b3167e0e91ee78
pwdjnyz11 <jyjyjy7418@gmail.com>
2023-01-06 10:18:10 +0900

    add button pressing event

--- app/src/main/java/org/techtown/miniproject/RecordActivity.java
+++ app/src/main/java/org/techtown/miniproject/RecordActivity.java
@@ -7,14 +7,14 @@ import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RecordActivity extends AppCompatActivity {
+    boolean game_index_state = false;
+    boolean user_index_state = false;
+    boolean score_index_state = false;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
 
-//        View blue_background = (View) findViewById(R.id.background_blue);
-//        View red_background = (View) findViewById(R.id.background_red);
-//        View yellow_background = (View) findViewById(R.id.background_yellow);
         ImageButton game_index = (ImageButton) findViewById(R.id.index_game);
         ImageButton user_index = (ImageButton) findViewById(R.id.index_user);
         ImageButton score_index = (ImageButton) findViewById(R.id.index_score);
@@ -22,27 +22,24 @@ public class RecordActivity extends AppCompatActivity {
         game_index.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                blue_background.setVisibility(View.VISIBLE);
-//                red_background.setVisibility(View.INVISIBLE);
-//                yellow_background.setVisibility(View.INVISIBLE);
+                game_index.setBackgroundResource(R.drawable.record_game_index_selector);
+                game_index_state = !game_index_state;
             }
         });
 
         user_index.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                blue_background.setVisibility(View.INVISIBLE);
-//                red_background.setVisibility(View.VISIBLE);
-//                yellow_background.setVisibility(View.INVISIBLE);
+                user_index.setBackgroundResource(R.drawable.record_user_index_selector);
+                user_index_state = !user_index_state;
             }
         });
 
         score_index.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                blue_background.setVisibility(View.INVISIBLE);
-//                red_background.setVisibility(View.INVISIBLE);
-//                yellow_background.setVisibility(View.VISIBLE);
+                score_index.setBackgroundResource(R.drawable.record_score_index_selector);
+                score_index_state = !score_index_state;
             }
         });
 
--- app/src/main/java/org/techtown/miniproject/RecordScoreActivity.java
+++ app/src/main/java/org/techtown/miniproject/RecordScoreActivity.java
@@ -1,6 +1,8 @@
 package org.techtown.miniproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,13 @@ public class RecordScoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_score);
+
+        ImageView back_btn = (ImageView) findViewById(R.id.back_button);
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
