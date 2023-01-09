82c6e6a3237b4be37abad0983ab2ffbf45eb2a8d
pwdjnyz11 <jyjyjy7418@gmail.com>
2023-01-09 18:30:58 +0900

    change design activity_record

--- app/src/main/java/org/techtown/miniproject/RecordActivity.java
+++ app/src/main/java/org/techtown/miniproject/RecordActivity.java
@@ -3,6 +3,7 @@ package org.techtown.miniproject;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -15,6 +16,7 @@ public class RecordActivity extends AppCompatActivity {
     RecyclerView game_recycler_view;
     RecyclerView user_recycler_view;
     RecyclerView score_recycler_view;
+    LinearLayout game_layout, user_layout, score_layout;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,22 +26,26 @@ public class RecordActivity extends AppCompatActivity {
         ImageButton user_index = (ImageButton) findViewById(R.id.index_user);
         ImageButton score_index = (ImageButton) findViewById(R.id.index_score);
 
+        game_layout = (LinearLayout) findViewById(R.id.game_layout);
+        user_layout = (LinearLayout) findViewById(R.id.user_layout);
+        score_layout = (LinearLayout) findViewById(R.id.score_layout);
+
         game_index.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (!game_index_state) {
-                    game_index.setImageResource(R.drawable.record_game_pressed_index);
+                    game_index.setImageResource(R.drawable.game_pressed_btn);
                     game_index_state = !game_index_state;
-                    game_recycler_view.setVisibility(View.VISIBLE);
-                    user_recycler_view.setVisibility(View.INVISIBLE);
-                    score_recycler_view.setVisibility(View.INVISIBLE);
+                    game_layout.setVisibility(View.VISIBLE);
+                    user_layout.setVisibility(View.INVISIBLE);
+                    score_layout.setVisibility(View.INVISIBLE);
 
                     if (user_index_state) {
-                        user_index.setImageResource(R.drawable.record_user_index);
+                        user_index.setImageResource(R.drawable.user_btn);
                         user_index_state = !user_index_state;
                     }
-                    if (score_index_state) {
-                        score_index.setImageResource(R.drawable.record_score_index);
+                    else if (score_index_state) {
+                        score_index.setImageResource(R.drawable.score_btn);
                         score_index_state = !score_index_state;
                     }
                 }
@@ -50,21 +56,20 @@ public class RecordActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (!user_index_state) {
-                    user_index.setImageResource(R.drawable.record_user_pressed_index);
+                    user_index.setImageResource(R.drawable.user_pressed_btn);
                     user_index_state = !user_index_state;
-                    user_recycler_view.setVisibility(View.VISIBLE);
-                    game_recycler_view.setVisibility(View.INVISIBLE);
-                    score_recycler_view.setVisibility(View.INVISIBLE);
+                    user_layout.setVisibility(View.VISIBLE);
+                    game_layout.setVisibility(View.INVISIBLE);
+                    score_layout.setVisibility(View.INVISIBLE);
 
                     if (game_index_state) {
-                        game_index.setImageResource(R.drawable.record_game_index);
+                        game_index.setImageResource(R.drawable.game_btn);
                         game_index_state = !game_index_state;
                     }
                     else if (score_index_state) {
-                        score_index.setImageResource(R.drawable.record_score_index);
+                        score_index.setImageResource(R.drawable.score_btn);
                         score_index_state = !score_index_state;
                     }
-
                 }
             }
         });
@@ -73,18 +78,18 @@ public class RecordActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (!score_index_state) {
-                    score_index.setImageResource(R.drawable.record_score_pressed_index);
+                    score_index.setImageResource(R.drawable.score_pressed_btn);
                     score_index_state = !score_index_state;
-                    score_recycler_view.setVisibility(View.VISIBLE);
-                    game_recycler_view.setVisibility(View.INVISIBLE);
-                    user_recycler_view.setVisibility(View.INVISIBLE);
+                    score_layout.setVisibility(View.VISIBLE);
+                    game_layout.setVisibility(View.INVISIBLE);
+                    user_layout.setVisibility(View.INVISIBLE);
 
                     if (game_index_state) {
-                        game_index.setImageResource(R.drawable.record_game_index);
+                        game_index.setImageResource(R.drawable.game_btn);
                         game_index_state = !game_index_state;
                     }
                     else if (user_index_state) {
-                        user_index.setImageResource(R.drawable.record_user_index);
+                        user_index.setImageResource(R.drawable.user_btn);
                         user_index_state = !user_index_state;
                     }
                 }
@@ -107,9 +112,9 @@ public class RecordActivity extends AppCompatActivity {
         user_recycler_view.setLayoutManager(user_layoutManager);
         UserAdapter user_adapter = new UserAdapter(getApplicationContext());
 
-        user_adapter.addItem(new UserItem("Gabe", 31, "♂", 4, 0));
-        user_adapter.addItem(new UserItem("June", 30, "♂", 0, 4));
-        user_adapter.addItem(new UserItem("Julia", 23, "♀", 2, 2));
+        user_adapter.addItem(new UserItem("Gabe", 31, "Male", 4, 0));
+        user_adapter.addItem(new UserItem("June", 30, "Male", 0, 4));
+        user_adapter.addItem(new UserItem("Julia", 23, "Female", 2, 2));
 
         user_recycler_view.setAdapter(user_adapter);
 
@@ -118,8 +123,8 @@ public class RecordActivity extends AppCompatActivity {
         score_recycler_view.setLayoutManager(score_layoutManager);
         ScoreAdapter score_adapter = new ScoreAdapter(getApplicationContext());
 
-        score_adapter.addItem(new ScoreItem("2022-10-18", "Gabe", "June", 4, 0));
-        score_adapter.addItem(new ScoreItem("2022-10-18", "Nowee", "Ann", 3, 1));
+        score_adapter.addItem(new ScoreItem(2022, "10/18", "Gabe", "June", 4, 0));
+        score_adapter.addItem(new ScoreItem(2022, "10/20", "Nowee", "Ann", 3, 1));
 
         score_recycler_view.setAdapter(score_adapter);
 
--- app/src/main/java/org/techtown/miniproject/ScoreItem.java
+++ app/src/main/java/org/techtown/miniproject/ScoreItem.java
@@ -2,26 +2,36 @@ package org.techtown.miniproject;
 
 public class ScoreItem {
 
-    String match_date;
+    int match_date_year;
+    String match_date_monthANDday;
     String user1_name;
     String user2_name;
     int user1_score;
     int user2_score;
 
-    public ScoreItem(String match_date, String user1_name, String user2_name, int user1_score, int user2_score) {
-        this.match_date = match_date;
+    public ScoreItem(int match_date_year, String match_date_monthANDday, String user1_name, String user2_name, int user1_score, int user2_score) {
+        this.match_date_year = match_date_year;
+        this.match_date_monthANDday = match_date_monthANDday;
         this.user1_name = user1_name;
         this.user2_name = user2_name;
         this.user1_score = user1_score;
         this.user2_score = user2_score;
     }
 
-    public String getMatch_date() {
-        return match_date;
+    public int getMatch_date_year() {
+        return match_date_year;
     }
 
-    public void setMatch_date(String match_date) {
-        this.match_date = match_date;
+    public void setMatch_date_year(int match_date_year) {
+        this.match_date_year = match_date_year;
+    }
+
+    public String getMatch_date_monthANDday() {
+        return match_date_monthANDday;
+    }
+
+    public void setMatch_date_monthANDday(String match_date_monthANDday) {
+        this.match_date_monthANDday = match_date_monthANDday;
     }
 
     public String getUser1_name() {
