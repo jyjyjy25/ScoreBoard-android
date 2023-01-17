b903aaabcf6c9ff1e46867a031708912b41bcfe6
pwdjnyz11 <jyjyjy7418@gmail.com>
2023-01-18 02:06:36 +0900

    add fragment through java code

--- app/src/main/java/org/techtown/miniproject/activities/InfoActivity.java
+++ app/src/main/java/org/techtown/miniproject/activities/InfoActivity.java
@@ -2,10 +2,14 @@ package org.techtown.miniproject.activities;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -13,7 +17,7 @@ import org.techtown.miniproject.GameFragment;
 import org.techtown.miniproject.R;
 import org.techtown.miniproject.UserFragment;
 
-public class InfoActivity extends FragmentActivity {
+public class InfoActivity extends AppCompatActivity {
     TabLayout tabs;
     GameFragment game_fragment;
     UserFragment user_fragment;
@@ -22,5 +26,28 @@ public class InfoActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
+        game_fragment = new GameFragment();
+        user_fragment = new UserFragment();
+
+        ImageButton game_index = (ImageButton) findViewById(R.id.index_game);
+        ImageButton user_index = (ImageButton) findViewById(R.id.index_user);
+
+        game_index.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, game_fragment).commit();
+            }
+        });
+
+        user_index.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, user_fragment).commit();
+            }
+        });
+
     }
+
+
+
 }
--- app/src/main/java/org/techtown/miniproject/activities/RecordActivity.java
+++ app/src/main/java/org/techtown/miniproject/activities/RecordActivity.java
@@ -18,93 +18,11 @@ import org.techtown.miniproject.adapters.UserAdapter;
 import org.techtown.miniproject.items.UserItem;
 
 public class RecordActivity extends AppCompatActivity {
-    boolean game_index_state = true;
-    boolean user_index_state = false;
-    boolean score_index_state = false;
     RecyclerView score_recycler_view;
-    LinearLayout game_layout, user_layout, score_layout;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
-
-        ImageButton game_index = (ImageButton) findViewById(R.id.index_game);
-        ImageButton user_index = (ImageButton) findViewById(R.id.index_user);
-        ImageButton score_index = (ImageButton) findViewById(R.id.index_score);
-
-        game_layout = (LinearLayout) findViewById(R.id.game_layout);
-        user_layout = (LinearLayout) findViewById(R.id.user_layout);
-        score_layout = (LinearLayout) findViewById(R.id.score_layout);
-
-        game_index.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!game_index_state) {
-                    game_index.setImageResource(R.drawable.game_pressed_btn);
-                    game_index_state = !game_index_state;
-                    game_layout.setVisibility(View.VISIBLE);
-                    user_layout.setVisibility(View.INVISIBLE);
-                    score_layout.setVisibility(View.INVISIBLE);
-
-                    if (user_index_state) {
-                        user_index.setImageResource(R.drawable.user_btn);
-                        user_index_state = !user_index_state;
-                    }
-                    else if (score_index_state) {
-                        score_index.setImageResource(R.drawable.score_btn);
-                        score_index_state = !score_index_state;
-                    }
-                }
-            }
-        });
-
-        user_index.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!user_index_state) {
-                    user_index.setImageResource(R.drawable.user_pressed_btn);
-                    user_index_state = !user_index_state;
-                    user_layout.setVisibility(View.VISIBLE);
-                    game_layout.setVisibility(View.INVISIBLE);
-                    score_layout.setVisibility(View.INVISIBLE);
-
-                    if (game_index_state) {
-                        game_index.setImageResource(R.drawable.game_btn);
-                        game_index_state = !game_index_state;
-                    }
-                    else if (score_index_state) {
-                        score_index.setImageResource(R.drawable.score_btn);
-                        score_index_state = !score_index_state;
-                    }
-                }
-            }
-        });
-
-        score_index.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!score_index_state) {
-                    score_index.setImageResource(R.drawable.score_pressed_btn);
-                    score_index_state = !score_index_state;
-                    score_layout.setVisibility(View.VISIBLE);
-                    game_layout.setVisibility(View.INVISIBLE);
-                    user_layout.setVisibility(View.INVISIBLE);
-
-                    if (game_index_state) {
-                        game_index.setImageResource(R.drawable.game_btn);
-                        game_index_state = !game_index_state;
-                    }
-                    else if (user_index_state) {
-                        user_index.setImageResource(R.drawable.user_btn);
-                        user_index_state = !user_index_state;
-                    }
-                }
-            }
-        });
-
-
-
-
 
         score_recycler_view = (RecyclerView) findViewById(R.id.score_recycler_view);
         LinearLayoutManager score_layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
