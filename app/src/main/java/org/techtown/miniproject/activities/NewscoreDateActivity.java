9206b4d3f091fdc6a4d263e006c099cff3750d89
pwdjnyz11 <jyjyjy7418@gmail.com>
2023-01-20 20:07:12 +0900

    add datepicker

--- app/src/main/java/org/techtown/miniproject/activities/NewscoreDateActivity.java
+++ app/src/main/java/org/techtown/miniproject/activities/NewscoreDateActivity.java
@@ -4,23 +4,31 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import org.techtown.miniproject.R;
+import org.w3c.dom.Text;
 
 public class NewscoreDateActivity extends AppCompatActivity {
+    int pYear=0, pMonth=0, pDay=0;
+    DatePickerDialog datePickerDialog;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,9 +37,28 @@ public class NewscoreDateActivity extends AppCompatActivity {
         /* 날짜 입력 버튼 */
         Button match_date = (Button) findViewById(R.id.match_date);
         match_date.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View view) {
-               // datePickerDialog 구현
+                /* 오늘 날짜 가져오기 */
+                Calendar calendar = Calendar.getInstance();
+                pYear = calendar.get(Calendar.YEAR);
+                pMonth = calendar.get(Calendar.MONTH);
+                pDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+                /* 버튼 클릭 시 datePickerDialog 띄우기 */
+                datePickerDialog = new DatePickerDialog(NewscoreDateActivity.this, R.style.MyDatePickerStyle,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                month = month+1;
+                                String date = year + ". " + month + ". " + dayOfMonth + ".";
+                                match_date.setText(date);
+                            }
+                        },pYear, pMonth, pDay);
+                datePickerDialog.show();
+                datePickerDialog.getButton(DatePickerDialog.BUTTON_NEGATIVE).setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.MainColorPrimary));
+                datePickerDialog.getButton(DatePickerDialog.BUTTON_POSITIVE).setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.MainColorPrimary));
             }
         });
 
