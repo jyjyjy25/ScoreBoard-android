9000790df8f8292c300d79095238697c453e20c2
pwdjnyz11 <jyjyjy7418@gmail.com>
2023-01-25 14:05:00 +0900

    add tab layout

--- app/src/main/java/org/techtown/miniproject/activities/InfoActivity.java
+++ app/src/main/java/org/techtown/miniproject/activities/InfoActivity.java
@@ -7,6 +7,7 @@ import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
@@ -26,23 +27,41 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         game_fragment = new GameFragment();
         user_fragment = new UserFragment();
 
-        ImageButton game_index = (ImageButton) findViewById(R.id.index_game);
-        ImageButton user_index = (ImageButton) findViewById(R.id.index_user);
+        getSupportFragmentManager().beginTransaction().add(R.id.container, game_fragment).commit();
 
-        game_index.setOnClickListener(new View.OnClickListener() {
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("게임"));
+        tabs.addTab(tabs.newTab().setText("유저"));
+
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View view) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, game_fragment).commit();
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+
+                Fragment selected = null;
+                if (position == 0) {
+                    selected = game_fragment;
+                } else if (position == 1) {
+                    selected = user_fragment;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
             }
-        });
 
-        user_index.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, user_fragment).commit();
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
 
