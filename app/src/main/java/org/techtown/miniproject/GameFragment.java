922ae01ee49e81e04eafd9bf172361e703e0d3e5
pwdjnyz11 <jyjyjy7418@gmail.com>
2023-01-25 16:58:34 +0900

    design tab_sort

--- app/src/main/java/org/techtown/miniproject/GameFragment.java
+++ app/src/main/java/org/techtown/miniproject/GameFragment.java
@@ -1,6 +1,7 @@
 package org.techtown.miniproject;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,6 +14,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.tabs.TabLayout;
 
 import org.techtown.miniproject.activities.InfoActivity;
 import org.techtown.miniproject.adapters.GameAdapter;
@@ -41,17 +44,20 @@ public class GameFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_game, container, false);
 
+        /* 선택된 tab 텍스트 색상 설정 */
+        TabLayout tabs = (TabLayout) rootView.findViewById(R.id.tabs);
+        tabs.setTabTextColors(Color.rgb(153, 153, 153), Color.rgb(0, 0, 0));
+
+        /* recyclerView 설정 */
         game_recycler_view = (RecyclerView) rootView.findViewById(R.id.game_recycler_view);
 
         LinearLayoutManager game_layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         game_recycler_view.setLayoutManager(game_layoutManager);
 
         GameAdapter game_adapter = new GameAdapter(getActivity());
-
         game_adapter.addItem(new GameItem("배구", 3));
         game_adapter.addItem(new GameItem("컬링", 9));
         game_adapter.addItem(new GameItem("허들", 4));
-
         game_recycler_view.setAdapter(game_adapter);
 
         return rootView;
--- app/src/main/java/org/techtown/miniproject/UserFragment.java
+++ app/src/main/java/org/techtown/miniproject/UserFragment.java
@@ -1,5 +1,6 @@
 package org.techtown.miniproject;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.tabs.TabLayout;
 
 import org.techtown.miniproject.adapters.UserAdapter;
 import org.techtown.miniproject.items.UserItem;
@@ -22,17 +25,20 @@ public class UserFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_user, container, false);
 
+        /* 선택된 tab 텍스트 색상 설정 */
+        TabLayout tabs = (TabLayout) rootView.findViewById(R.id.tabs);
+        tabs.setTabTextColors(Color.rgb(153, 153, 153), Color.rgb(0, 0, 0));
+
+        /* recyclerView 설정 */
         user_recycler_view = (RecyclerView) rootView.findViewById(R.id.user_recycler_view);
 
         LinearLayoutManager user_layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         user_recycler_view.setLayoutManager(user_layoutManager);
 
         UserAdapter user_adapter = new UserAdapter(getActivity());
-
         user_adapter.addItem(new UserItem("Gabe", 31, "Male", 4, 0));
         user_adapter.addItem(new UserItem("June", 30, "Male", 0, 4));
         user_adapter.addItem(new UserItem("Julia", 23, "Female", 2, 2));
-
         user_recycler_view.setAdapter(user_adapter);
 
         return rootView;
