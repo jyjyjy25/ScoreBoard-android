3c065c8371fa506f06c2fe4c8c8142805ba00365
pwdjnyz11 <jyjyjy7418@gmail.com>
2023-02-16 22:31:03 +0900

    add dropdown menubar at activity_record

--- app/src/main/java/org/techtown/miniproject/activities/RecordActivity.java
+++ app/src/main/java/org/techtown/miniproject/activities/RecordActivity.java
@@ -3,14 +3,19 @@ package org.techtown.miniproject.activities;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.textfield.TextInputLayout;
 
 import org.techtown.miniproject.adapters.GameAdapter;
 import org.techtown.miniproject.items.GameItem;
@@ -22,6 +27,9 @@ import org.techtown.miniproject.items.UserItem;
 
 public class RecordActivity extends AppCompatActivity {
     RecyclerView score_recycler_view;
+    Spinner spinner;
+    AutoCompleteTextView autoCompleteTextview;
+    TextInputLayout textInputLayout;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,10 +45,44 @@ public class RecordActivity extends AppCompatActivity {
         score_recycler_view.setLayoutManager(score_layoutManager);
         ScoreAdapter score_adapter = new ScoreAdapter(getApplicationContext());
 
-        score_adapter.addItem(new ScoreItem("2022/10/18", "가위바위보", "Gabe", "June", 4, 0));
-        score_adapter.addItem(new ScoreItem("2022/11/21", "탁구", "Nowee", "Ann", 3, 1));
+        score_adapter.addItem(new ScoreItem("2022. 10. 18.", "가위바위보", "Gabe", "June", 4, 0));
+        score_adapter.addItem(new ScoreItem("2022. 11. 21.", "탁구", "Nowee", "Ann", 3, 1));
 
         score_recycler_view.setAdapter(score_adapter);
+
+        /* 스피너 설정 */
+//        spinner = findViewById(R.id.spinner);
+//
+//        ArrayAdapter itemAdapter = ArrayAdapter.createFromResource(this, R.array.game, android.R.layout.simple_spinner_dropdown_item);
+//        itemAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner.setAdapter(itemAdapter);
+//
+//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) { }
+//        });
+
+        String[] GAMES = new String[] {"전체", "가위바위보", "탁구"};
+
+        textInputLayout = findViewById(R.id.dateFilterContainer);
+        autoCompleteTextview = findViewById(R.id.datesFilterSpinner);
+
+        ArrayAdapter<String> itemAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, GAMES);
+        autoCompleteTextview.setAdapter(itemAdapter);
+
+        autoCompleteTextview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
+
 
     }
 }
